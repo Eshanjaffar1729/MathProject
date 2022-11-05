79d87ae46dc633eb09b1adee82d0865530704c0f
Eshanjaffar1729 <62180338+Eshanjaffar1729@users.noreply.github.com>
2022-11-05 18:45:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from itertools import count
+from matplotlib.animation import FuncAnimation
 from matplotlib import pyplot as plt
 import openpyxl
 import math
 
 
+
+#Defining some funcitons for statisctics
+
+#opening Excel sheet and creating an Excel Object
+path = "v.xlsx"
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+
+
+#defining Some useful variables and lists
+X = []
+x = []
+Y = []
+l = []
+R = []
+be = 2
+e = 334
+MA =[]
 
 #Defining some funcitons for statisctics
 def sigma_x(x):
@@ -23,26 +43,17 @@ def sigma_x2(x):
         ans += elems**2
     return ans
 
-#Function to reverse a list
-def reverse(x):
-    z = []
-    for i in range(len(x)-1,-1,-1):
-        z.append(x[i])
-    return z
 
-#opening Excel sheet and creating an Excel Object
-path = "v.xlsx"
-wb_obj = openpyxl.load_workbook(path)
-sheet_obj = wb_obj.active
+for i in range(be,e):
+    cell_obj = sheet_obj.cell(row = i, column = 1)
+    x.append(str(cell_obj.value))
 
-
-#defining Some useful variables and lists
-X = []
-Y = []
-l = []
-be = 2
-e = 100
-MA =[]
+for i in range(0,len(x)):
+    ans = ""
+    for j in range(0,9):
+        ans += x[i][j]
+    x[i] = ans
+    ans =""
 
 for i in range(be,e):
     X.append(i)
@@ -50,11 +61,8 @@ for i in range(be,e):
 for i in range(be,e):
     cell_obj = sheet_obj.cell(row = i, column = 2)
     Y.append(cell_obj.value)
+Y.reverse()
 
-
-Y = reverse(Y)
-
-#Finding the slope and y intersept of least squares or regression line
 n = e-be
 m = (n*sigma_xy(X,Y) - (sigma_x(X)*sigma_x(Y)))/(n*sigma_x2(X) - sigma_x(X)**2)
 b = (sigma_x(Y) -m*sigma_x(X))/n
@@ -71,10 +79,29 @@ for i in range(0,n):
         MA.append(sigma_x(ra)/mas)
     ra=[]
 
-y1,x1 = [m+b,m*n+b],[1,n]
-fig, ax = plt.subplots()
-ax.plot(X,Y)
-ax.plot(x1,y1,color="red")
-ax.plot(X,MA,color="green")
-fig.show()
+for i in range(0,n):
+    R.append(m*X[i]+b)
 
+x_data= []
+y_data= []
+ma_data =[]
+r_data = []
+fig,ax = plt.subplots()
+counter = count(0,1)
+
+def update(i):
+    idx=next(counter)
+    x_data.append(X[i])
+    y_data.append(Y[i])
+    r_data.append(R[i])
+    ma_data.append(MA[i])
+    plt.cla()
+    ax.plot(x_data,y_data,"-b",label="Share price")
+    ax.plot(x_data,ma_data,"-g",label="Moving averages")
+    ax.plot(x_data,r_data,"-r",label="Regression line")
+    ax.set_xlabel("Day Number")
+    ax.set_ylabel("Share price")  
+    ax.legend(loc="upper right")
+    
+ani = FuncAnimation(fig=fig,func=update,interval=100)
+plt.show()
